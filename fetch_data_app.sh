88cd5b8e8c07a3ae3c58ded839509bc684083743
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2025-03-18 15:08:26 +0100

    requirements

--- fetch_data_app.sh
+++ fetch_data_app.sh
@@ -5,8 +5,8 @@ rm -rf okar-static-dev
 wget https://github.com/OKA-Rechnungen/okar-static/archive/refs/heads/dev.zip
 unzip dev
 mv okar-static-dev/*.* ./
-mv okar-static-dev/xslt/ ./xslt
 mv okar-static-dev/html/ ./html
 mv okar-static-dev/build_app/ ./build_app
+mv okar-static-dev/config/requirements.txt .
 rm -rf okar-static-dev
 rm dev.zip
